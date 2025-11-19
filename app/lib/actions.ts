97bc0a2e02a4ae222b1c6b794955943c8c1b58ec
hisami <koron.take@gmail.com>
2025-11-19 23:07:11 +0900

    mutation

---
+++ app/lib/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import postgres from "postgres";
+import { z } from "zod";
+
+const sql = postgres(process.env.POSTGRES_URL ?? "", { ssl: "require" });
+
+const FormSchema = z.object({
+	id: z.string(),
+	customerId: z.string(),
+	amount: z.coerce.number(),
+	status: z.enum(["paid", "pending"]),
+	date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export const createInvoice = async (data: FormData) => {
+	const { customerId, amount, status } = CreateInvoice.parse({
+		customerId: data.get("customerId"),
+		amount: data.get("amount"),
+		status: data.get("status"),
+	});
+
+	const amountInCents = amount * 100;
+	const date = new Date().toISOString().split("T")[0];
+
+	await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+};
