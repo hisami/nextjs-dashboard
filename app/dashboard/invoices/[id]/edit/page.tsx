e52d2ba6e9c4e4cb6d853b8d055c71bc17cf0030
hisami <koron.take@gmail.com>
2025-11-20 11:34:02 +0900

    エラーハンドリング

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
@@ -9,6 +10,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 		fetchInvoiceById(id),
 		fetchCustomers(),
 	]);
+
+	if (!invoice) {
+		notFound();
+	}
+
 	return (
 		<main>
 			<Breadcrumbs
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,10 +28,15 @@ export const createInvoice = async (data: FormData) => {
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split("T")[0];
 
-	await sql`
+	try {
+		await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+	} catch (error) {
+		console.error(error);
+		// return { message: "Database error: Failed to create invoice." };
+	}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
@@ -46,10 +51,15 @@ export const updateInvoice = async (id: string, data: FormData) => {
 
 	const amountInCents = amount * 100;
 
-	await sql`
+	try {
+		await sql`
     UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+	} catch (error) {
+		console.error(error);
+		// return { message: "Database error: Failed to update invoice." };
+	}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
